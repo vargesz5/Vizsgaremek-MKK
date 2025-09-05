22c14cb438e7a15ce123670af421daf3ea94e71d
HKristof25 <21.hajdu.kristof@mechwart.com>
2025-09-05 14:17:34 +0200

    home page progress

--- Frontend/boraszat/src/App.tsx
+++ Frontend/boraszat/src/App.tsx
@@ -3,12 +3,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import SuLi from '../Msrc/SuLi'
+import Home from '../Ksrc/Home'
 
 function App() {
 
   return (
     <>
-     <SuLi/>
+     
     </>
   )
 }
